8b3445fcaca0ebbd155f57d6c0db9dd57af3c926
Kipa Tachak <ktachak@gmail.com>
2018-12-26 15:24:16 +0530

    data format specification added which was omitted before

--- app/lib/data_frmt.py
+++ app/lib/data_frmt.py
@@ -16,23 +16,46 @@ objects and retrieve relevant info from the object.
 """
 
 import json
+from enum import Enum
 
 
-def check_field(dtfrmt, obj):
-    """Recursively check that various fields in data_format
-    match that of obj.
+class FormatTypes(Enum):
+    """Various format types for data exchange."""
+    
+    CLIENT_INIT = 1
+    USR_REGISTRATION = 2
+    # TODO
 
-    return True if fields match otherwise False."""
+    @staticmethod
+    def check_field(dtfrmt, obj):
+        """Recursively check that various fields in data_format
+        match that of obj.
 
-    if type(dtfrmt) is not dict:
-        return type(obj) is dtfrmt
+        return True if fields match otherwise False."""
 
-    for field in obj:
-        if field not in dtfrmt: return False
-        if not check_field(dtfrmt[field], obj[field]):
-            return False
+        if type(dtfrmt) is not dict:
+            return type(obj) is dtfrmt
 
-    return True
+        for field in obj:
+            if field not in dtfrmt: return False
+            if not check_field(dtfrmt[field], obj[field]):
+                return False
+
+        return True
+
+    
+DATA_EXCHANGE_FORMATS = {
+    FormatTypes.CLIENT_INIT: {
+        'c_info': {
+            'c_conn' : {
+                'ip': str,
+                'port' : int,
+                },
+            'user_id': str,
+            },
+        'to_peers': list,
+        },
+    }
 
 
 class DataFormatError(Exception):
@@ -44,24 +67,15 @@ class DataFormatError(Exception):
         
 
 
-class ParseClientInitData():
-    """Parse the json data in client initial contact
-    with the server.
-    
-    Throw ClientDataInvalidError if the format is wrong.
+class DataFormatSpecs():
+    """Data format specification for communication between
+    the client and the server.
     """
-    data_format = {
-        'client_info': {
-            'conn_info' : {
-                'ip': str,
-                'port' : int,
-                },
-            'user_id': str,
-            },
-        'to_peers': list,
-        }
-        
-    def __init__(self, client_json_data, ignore_auth=False, ignore_topeers=False):
+
+    def __init__(self, frmt_type):
+        self.frmt_type = frmt_type
+    
+    def parse_client_data(self, client_json_data):
         """Parse client_json_data and set appropriate
         fields in the class.
         
@@ -70,16 +84,27 @@ class ParseClientInitData():
         Fields:
         1. client_addr: set to client address.
         2. client_port: set to client port.
-        3. client_user_id: set to client user_id.
+        3. client_uuid: set to client user_id.
         4. snd_to_peers: list of peer ids to send message.
         """
         try:
             client_json_obj = json.loads(client_json_data)
         except:
-            raise DataFormatError('client data not in proper format:\n{}'
-                                  .format(client_json_data))
+            raise DataFormatError(client_json_obj)
         
         #fields_match = check_field(data_format, client_json_obj)
         #if not fields_match: #raise exception
-        dfrmt_ok = check_field(data_format, client_json_obj)
-        if not dfrmt_ok: raise DataFormatError(
+        dfrmt_ok = check_field(DATA_EXCHANGE_FORMATS[self.frmt_type]
+                               , client_json_obj)
+        if not dfrmt_ok:
+            raise DataFormatError(client_json_obj)
+
+        client_info = client_json_obj['c_info']
+        self.client_addr = client_info['c_conn']['ip']
+        self.client_port = client_info['c_conn']['port']
+        self.client_uuid = client_info['uuid']
+        self.snd_to_peers = client_json_obj['to_peers']
+
+        if len(self.snd_to_peers) > 0:
+            if type(self.snd_to_peers[0]) is not str:
+                raise DataFormatError(client_json_obj)
